340c6a70a4bdd8ae15cab86645a33c99fd4467e5
jeandiallo <147546631+jeandiallo@users.noreply.github.com>
2024-11-30 22:51:21 +0000

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [
+    (module.exports = {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    }),
+  ],
+};
